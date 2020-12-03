cf032366d67b743408453a38b88b82b9ab709de9
DavidIMSER <d.medina@imserltda.com>
2020-12-03 09:35:25 -0300

    update app chofer

--- app_chofer/php/connection.php
+++ app_chofer/php/connection.php
@@ -1,8 +1,8 @@
 <?php
 	$server = "localhost";
 	$user = "root";
-	$password = "desarrollo.toexpress.2019";
-  	#$password = "123ewq";
+	#$password = "desarrollo.toexpress.2019";
+  	$password = "123ewq";
   	$bd = "logistic_transporte";
 	$secret = "c85ae6f5bbf337301e33bb5ee0d13f9a7a3e2148";
 
